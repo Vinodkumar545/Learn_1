f76980b2935ba35ae3a241a790138cbfeb8f3a67
vinodkumar545 <vinodkumar.kouthal@stride.ai>
2018-11-05 23:16:37 +0530

    First Commit

--- test_execute.py
+++ test_execute.py
@@ -15,3 +15,5 @@ def test_001_login_with_right_credential(driver):
 
 def test_002_login_with_incorrect_credential(driver):
 	assert login_page.login(driver, URL, "right_email_id", "incorrect_password") is False
+
+
